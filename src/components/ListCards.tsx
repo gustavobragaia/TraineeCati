c26d7e38dc43c10f3dd8f6a0bab2bc0e0e40ce3e
Gustavo Bragaia <52480533+gustavobragaia@users.noreply.github.com>
2025-07-01 02:55:09 -0300

    feat: components of listcard and taskcard

---
+++ src/components/ListCards.tsx
@@ -0,0 +1,54 @@
+import { ThreeDots } from 'react-bootstrap-icons'
+import styles from './ListCards.module.css'
+import { TaskCard } from './TaskCard.tsx'
+
+const tasks: {id: number; taskTitle: string; taskDescription: string; taskDate: Date; priorityStatus: string;}[] = [
+    {
+        id: 1,
+        priorityStatus: "Baixa prioridade",
+        taskTitle: "Como jogar no Japão",
+        taskDescription: "Migos the classic of the music lil Bro",
+        taskDate: new Date('2025-05-03 20:00:00'),
+    },
+    {
+        id: 2,
+        priorityStatus: "Média prioridade",
+        taskTitle: "Como jogar no Afeganistão",
+        taskDescription: "Migos the classic of the music lil Bro",
+        taskDate: new Date('2025-05-03 20:00:00'),
+    },
+    {
+        id: 3,
+        priorityStatus: "Baixa prioridade",
+        taskTitle: "Como jogar no Jalapão",
+        taskDescription: "Migos the classic of the music lil Bro",
+        taskDate: new Date('2025-05-03 20:00:00'),
+    }
+]
+
+export function ListCards(){
+    return(
+        <div className={styles.listCard}>  
+            <div className={styles.listHeader}>
+                <h6>A fazer</h6>
+                <ThreeDots color='white' size={24}></ThreeDots>
+            </div>
+
+            <div className={styles.listTasks}>
+                {tasks.map(task => {
+                    return(
+                        <TaskCard
+                            key={task.id}
+                            priorityStatus={task.priorityStatus}
+                            taskTitle={task.taskTitle}
+                            taskDescription={task.taskDescription}
+                            taskDate={task.taskDate}
+                        />
+                    )
+                })}
+            </div>
+
+            <div className={styles.newTaskFooter}></div>
+        </div>
+    )
+}
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,46 @@
-export function TaskCard(){
-    
+import { CalendarWeekFill, Check2 } from 'react-bootstrap-icons'
+import {format} from 'date-fns'
+import styles from './TaskCard.module.css'
+
+ type TaskCardProps = {
+    id: number;
+    priorityStatus: string;
+    taskTitle: string,
+    taskDescription: string,
+    taskDate: Date,
+}
+
+export function TaskCard(props: TaskCardProps){
+
+    const formatedDate = format(props.taskDate,  "d 'de' LLLL 'ás' HH:mm'h'")
+
+    return(
+        <div className={styles.wrapper}>
+
+            <div className={styles.headerCardTask}>
+                <div className={styles.priority}>
+                    <span className={styles.priorityText}>{props.priorityStatus}</span>
+                </div>
+                <div className={styles.finishedTask}>
+                    <Check2 
+                        color='white' 
+                        size={24} 
+                        style={{ border: '1px dashed #fff', borderRadius: '50%', padding: '3px' }}>
+                    </Check2>
+                    <span>Finalizar</span>
+                </div>
+            </div>
+
+            <div className={styles.contentCardTask}>
+                <h6>{props.taskTitle}</h6>
+                <p>{props.taskDescription}</p>
+            </div>
+
+            <div className={styles.footerCardTask}>
+                <CalendarWeekFill color='var(--date-text)'></CalendarWeekFill>
+                <time>{formatedDate}</time>
+            </div>
+        </div>  
+    )
+
 }
